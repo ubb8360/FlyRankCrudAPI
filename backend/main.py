bd33cf1c228f1ad3464843929bde1a38be902e6f
Uday Bista <ubb8360@rit.edu>
2026-07-22 16:02:32 -0400

    Fixed "/" in main

--- backend/main.py
+++ backend/main.py
@@ -3,16 +3,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/tasks")
+@app.get("/")
 async def root():
     return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"], }
 
 @app.get("/health")
 async def health_check():
         return {"status": "healthy"}
+    
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+
+
     
